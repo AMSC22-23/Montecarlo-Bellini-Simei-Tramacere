5d3469e45dbc1b8c237bbd9d4ce08360cccedebc
luca-simei <luca.simei@mail.polimi.it>
2023-11-17 22:21:01 +0100

    minor fixes, TODO: parallelization

--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <string>
+
 #include "semicircle_MCintegration.hpp"
 
 
 int main() {
 
-    int n;
-    //int dim;
-    double radius=21.0;
+    int dim;
 
-    std::cout << "How many points do you want to generate?" << std::endl;
-    std::cin >> n;
-    //std::cout << "In how many dimensions your domain of integration is?" << std::endl;
-    //std::cin >> dim;
+    std::cout << "In how many dimensions your domain of integration is?" << std::endl;
+    std::cin >> dim;
 
-    MC_integration(n,radius);
+    MC_integration(dim);
 
     return 0;
 }
--- semicircle_MCintegration.hpp
+++ semicircle_MCintegration.hpp
@@ -4,28 +4,33 @@
 #include <chrono>
 
 
-int MC_integration(int n, double radius) {
+int MC_integration(int dim) {
 
-        double x, y;
-        double integral;
+        int n=1000000;
+        double radius=21.0;
+
+        double x[2];
+        double omega,integral;
         double sum=0;
 
         std::random_device rd;
+        auto seed = rd();
 
-        std::default_random_engine eng( rd() );
+        std::default_random_engine eng(seed);
 
         auto start = std::chrono::high_resolution_clock::now(); // start the timer
 
         for( int i = 0; i < n; ++i ) {
                 std::uniform_real_distribution<double> distribution(0.0,radius);
-                x = distribution(eng);
-                y = distribution(eng);
-                if ( pow(x,2) + pow(y,2) <= pow(radius,2) ) {
+                x[0] = distribution(eng);
+                x[1] = distribution(eng);
+                if ( pow(x[0],2) + pow(x[1],2) <= pow(radius,2) ) {
                         sum += 1;
                 }
         }
 
-        integral = (2.0*radius*radius/(double)n)*(sum);
+        omega = 2.0*pow(radius,2);
+        integral = (omega/static_cast<double>(n))*sum;
         std::cout << "--------------------------------------------------------" << std::endl;
         std::cout << "The approximate result of your integral is: " << integral << std::endl;
 
