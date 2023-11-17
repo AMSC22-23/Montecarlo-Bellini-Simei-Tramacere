9324a11f8b6f487ede95acb925189ed800696163
trama00 <luca.tramacere@gmail.com>
2023-11-17 18:58:30 +0000

    processing time added

--- semicircle_MCintegration.hpp
+++ semicircle_MCintegration.hpp
@@ -1,6 +1,7 @@
 #include <random>
 #include <iostream>
 #include <cmath>
+#include <chrono>
 
 
 int MC_integration(int n, double radius) {
@@ -12,6 +13,9 @@ int MC_integration(int n, double radius) {
         std::random_device rd;
 
         std::default_random_engine eng( rd() );
+
+        auto start = std::chrono::high_resolution_clock::now(); // start the timer
+
         for( int i = 0; i < n; ++i ) {
                 std::uniform_real_distribution<double> distribution(0.0,radius);
                 x = distribution(eng);
@@ -24,6 +28,10 @@ int MC_integration(int n, double radius) {
         integral = (2.0*radius*radius/(double)n)*(sum);
         std::cout << "--------------------------------------------------------" << std::endl;
         std::cout << "The approximate result of your integral is: " << integral << std::endl;
+
+        auto end = std::chrono::high_resolution_clock::now(); // stop the timer
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start); // calculate the duration
+        std::cout << "Processing time: " << duration.count() << " microseconds" << std::endl; // print the duration
 
         return 0;
 }
