056c29c706bb159717b8b41b465996a849724775
luca-simei <luca.simei@mail.polimi.it>
2023-12-15 17:35:00 +0100

    Totally new code. Implemented abstract class Geometry and class HyperSphere. Implemented Montecarlo_integration function inside main source file.

---
+++ Geometry.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+
+
+class Geometry
+{
+    public:
+        Geometry() = default;
+        
+        virtual std::vector<double> generate_random_point() = 0;
+        virtual void calculate_volume() = 0;
+        virtual void calculate_approximated_volume(int) = 0;
+
+        int get_dimension() const { return dimension; }
+        double get_volume() const { return volume; }
+        double get_approximated_volume() const { return approximated_volume; }
+        std::string get_function() const { return function; }
+
+    protected:
+        int dimension;
+        double volume;
+        double approximated_volume;
+        std::string function;
+};
--- main.cpp
+++ main.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
+#include <vector>
 #include <string>
-#include "MCintegration_with_user_data.hpp"
-#include "nD_sphere_MCintegration.hpp"
-#include "sphere_volume.hpp"
+#include <chrono>
 
+#include "HyperSphere.cpp"
+
+
+// note that the following method implements rn only the special case with the function f(x) = 1
+// TODO: generalize the method for all the functions
+
+std::pair<double, double> Montecarlo_integration(int n, int dim, double rad) {
+    // setup the hypersphere
+    HyperSphere hypersphere(dim, rad);
+    // start the timer
+    auto start = std::chrono::high_resolution_clock::now();
+    // generate the random points
+    for (int i = 0; i < n; ++i) {
+        std::vector<double> random_point = hypersphere.generate_random_point();
+        if (!random_point.empty()) hypersphere.add_point_inside();
+    }
+    // calculate the integral
+    hypersphere.calculate_approximated_volume(n);
+    double integral = hypersphere.get_approximated_volume();
+    // stop the timer
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    return std::make_pair(integral, duration.count());
+}
 
 
 int main() {
-    
-    // Test cases
-    std::vector<int> dim(6);
-    dim = {1, 2, 3, 4, 5, 6};
-    std::pair<double, double> result;
-    double volume;
+    int n = 1000000;
+    int dim = 2;
+    double rad = 1.0;
 
-    for (int i = 0; i < 6; i++)
-    {
-        result = nD_sphere_MC_integration(dim[i]);
-        std::cout << "The approximate result in " << dim[i] << " dimensions of your integral is: " << result.first << std::endl;
-        std::cout << "The time needed to calculate the integral is: " << result.second << " microseconds" << std::endl;
-        volume = sphere_volume(dim[i], 1.0);
-        std::cout << "The exact result in " << dim[i] << " dimensions of your integral is: " << volume << std::endl;
-        std::cout << "The absolute error is: " << std::abs(result.first - volume) << std::endl;
-        std::cout << "The relative error is: " << std::abs(result.first - volume) / volume << std::endl << std::endl;
-    }
+    HyperSphere hypersphere(dim, rad);
 
-    // User input (more dynamic code)
-    int n; // TODO fare una ENUM con LOW, MEDIUM, HIGH per il numero di punti, che indicano l'accuratezza dell'integrale
-    double domain_bound;
-    int user_given_dim;
-
-    std::cout << "What is the upper bound of the integration domain? (note that the domain of integration is symmetric)" << std::endl;
-    std::cin >> domain_bound;
-    std::cout << "How many points do you want to generate?" << std::endl;
-    std::cin >> n;
-    std::cout << "In how many dimensions your domain of integration is?" << std::endl;
-    std::cin >> user_given_dim;
-
-    MC_integration(n,domain_bound, user_given_dim);
+    std::pair<double, double> result = Montecarlo_integration(n, dim, rad);
+    std::cout << "The approximate result in " << dim << " dimensions of your integral is: " << result.first << std::endl;
+    std::cout << "The time needed to calculate the integral is: " << result.second << " microseconds" << std::endl;
+    hypersphere.calculate_volume();
+    double volume = hypersphere.get_volume();
+    std::cout << "The exact result in " << dim << " dimensions of your integral is: " << volume << std::endl;
+    std::cout << "The absolute error is: " << std::abs(result.first - volume) << std::endl;
+    std::cout << "The relative error is: " << std::abs(result.first - volume) / volume << std::endl << std::endl;
 
     return 0;
 }
