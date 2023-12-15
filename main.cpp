8000833375a9f85e21454a9f3a22ba5568ef66b7
luca-simei <luca.simei@mail.polimi.it>
2023-12-15 17:45:18 +0100

    main modified. Now the user is free to choose some parameters.

--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@
 // note that the following method implements rn only the special case with the function f(x) = 1
 // TODO: generalize the method for all the functions
 
-std::pair<double, double> Montecarlo_integration(int n, int dim, double rad) {
-    // setup the hypersphere
-    HyperSphere hypersphere(dim, rad);
+std::pair<double, double> Montecarlo_integration(HyperSphere& hypersphere, int n) {
     // start the timer
     auto start = std::chrono::high_resolution_clock::now();
     // generate the random points
@@ -29,21 +27,26 @@ std::pair<double, double> Montecarlo_integration(int n, int dim, double rad) {
 }
 
 
-int main() {
-    int n = 1000000;
-    int dim = 2;
-    double rad = 1.0;
-
+int main(int argc, char** argv) {
+    int n, dim;
+    double rad;
+    // ask the user to insert the parameters
+    std::cout << "Insert the number of random points to generate: ";
+    std::cin >> n;
+    std::cout << "Insert the dimension of the hypersphere: ";
+    std::cin >> dim;
+    std::cout << "Insert the radius of the hypersphere: ";
+    std::cin >> rad;
+    // generate the hypersphere
     HyperSphere hypersphere(dim, rad);
-
-    std::pair<double, double> result = Montecarlo_integration(n, dim, rad);
-    std::cout << "The approximate result in " << dim << " dimensions of your integral is: " << result.first << std::endl;
+    // calculate the integral and print the results
+    std::pair<double, double> result = Montecarlo_integration(hypersphere, n);
+    std::cout << std::endl << "The approximate result in " << dim << " dimensions of your integral is: " << result.first << std::endl;
     std::cout << "The time needed to calculate the integral is: " << result.second << " microseconds" << std::endl;
     hypersphere.calculate_volume();
     double volume = hypersphere.get_volume();
     std::cout << "The exact result in " << dim << " dimensions of your integral is: " << volume << std::endl;
     std::cout << "The absolute error is: " << std::abs(result.first - volume) << std::endl;
-    std::cout << "The relative error is: " << std::abs(result.first - volume) / volume << std::endl << std::endl;
-
+    std::cout << "The relative error is: " << std::abs(result.first - volume) / volume << std::endl;
     return 0;
 }
