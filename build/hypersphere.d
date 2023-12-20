dbd2cf42383fe57fabe049ccc770d04a3dad332f
luca-simei <luca.simei@mail.polimi.it>
2023-12-20 16:11:53 +0100

    make clean

--- build/hypersphere.d
+++
@@ -1,9 +0,0 @@
-build/hypersphere.o: src/hypersphere.cpp \
-  /usr/local/opt/libomp/include/omp.h include/project/hypersphere.hpp \
-  include/project/geometry.hpp
-
-/usr/local/opt/libomp/include/omp.h:
-
-include/project/hypersphere.hpp:
-
-include/project/geometry.hpp:
--- build/input_manager.d
+++
@@ -1,4 +0,0 @@
-build/input_manager.o: src/input_manager.cpp \
-  include/project/input_manager.hpp
-
-include/project/input_manager.hpp:
